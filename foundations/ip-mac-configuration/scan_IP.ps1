2c916c84532a4d01937fd49859b67aa9110a607e
Keith Thomson <thomsonk930@gmail.com>
2026-09-24 15:06:57 -0400

    Add comment to scan_IP.ps1 for clarity
    Added a comment to clarify the script's purpose.

--- foundations/ip-mac-configuration/scan_IP.ps1
+++ foundations/ip-mac-configuration/scan_IP.ps1
@@ -1,4 +1,4 @@
-
+# For each IP that as the same subnet, return the IP address and Link Layer.
 
 1..254 | ForEach-Object { Test-Connection -ComputerName "192.168.1.$_" -Count 1 -TimeoutMilliSecs 100 -Quiet }
 Get-NetNeighbor | Where-Object { $_.IPAddress -like "192.168.1.*" } | Select-Object IPAddress, LinkLayerAddress
